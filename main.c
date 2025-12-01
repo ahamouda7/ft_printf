77489f2245ea32ba4d18d9c5a40e5b12c2944f75
Anass <12345678+yourusername@users.noreply.github.com>
2025-12-01 22:00:49 +0100

    Done

--- main.c
+++ main.c
@@ -3,32 +3,30 @@
 
 int main()
 {
-	// ft_printf("%d\n", 2147483647); // correct 100%
-	// printf("%d\n", 2147483647);
+	// ft_printf("[%d bytes]", ft_printf("%d\n", 2147483647));
+	// printf("[%d bytes]", printf("%d\n", 2147483647));
 
-	// ft_printf("%i\n", -29); // correct 100%
-	// printf("%i\n", -29);
+	// ft_printf("[%d bytes]", ft_printf("%i\n", -29));
+	// printf("[%d bytes]", printf("%i\n", -29));
 
-	// ft_printf("%u\n", -4294967295); // correct 100%
-	// printf("%u\n", -4294967295);
+	// ft_printf("[%d bytes]", ft_printf("%u\n", -4294967295));
+	// printf("[%d bytes]", printf("%u\n", -4294967295));
 
-	// ft_printf("%c\n", 'A'); // correct 100%
-	// printf("%c\n", 'A');
+	// ft_printf("[%d bytes]", ft_printf("%c\n", 'A'));
+	// printf("[%d bytes]", printf("%c\n", 'A'));
 	
-	// ft_printf("%s\n", NULL); // correct 100%
-	// printf("%s", NULL);
-	
-	// ft_printf(" %x \n", 16); // correct 100%
-	// printf("[%d bytes]\n", ft_printf(" %x ", -10));
-	// printf(" %x ", -10);
+	// ft_printf("[%d bytes]", ft_printf("%s\n", NULL));
+	// ft_printf("[%d bytes]", ft_printf("%s\n", NULL));
 
-	// ft_printf("%X\n", -4294967295); // correct 100%
-	// printf("%X\n", -4294967295);
+	// ft_printf("[%d bytes]", ft_printf("%x", -10));
+	// printf("[%d bytes]", printf("%x", -10));
 
-	// ft_printf("%p\n", 0); // correct 100%
-	printf("[%d bytes]", ft_printf(" %p %p ", 0, 0));
-	// printf("[%d bytes]", printf(" %p %p ", 0, 0));
+	// ft_printf("[%d bytes]", ft_printf("%X", -4294967295));
+	// printf("[%d bytes]", printf("%X", -4294967295));
 
-	// ft_printf("%y\n", "Anass"); // correct 100%
-	// printf("%y\n", "Anass");
+	ft_printf("[%d bytes]\n", ft_printf("%p", NULL));
+	printf("[%d bytes]\n", printf("%p", NULL));
+
+	// ft_printf("[%d bytes]", ft_printf("%y\n", "Anass"));
+	// printf("[%d bytes]", printf("%y\n", "Anass"));
 }
